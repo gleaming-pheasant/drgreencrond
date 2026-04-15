584111851ccd307e78c3aeb9a3b4e5ab5b0599f3
gleaming-pheasant <patrick.mchugh@york.ac.uk>
2026-04-15 17:04:15 +0100

    Create initial signal registry, initialise journdald tracing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,96 @@
 mod forecast;
 
-fn main() {
-    let forecast = forecast::Forecast::fetch_fw_24h_postcode(
-        "YO1", time::UtcDateTime::now()
-    ).unwrap();
+use std::os::unix::io::AsRawFd;
 
-    println!("{:#?}", forecast);
+use mio::{Events, Poll, Token, Interest};
+use mio::unix::SourceFd;
+use nix::sys::signal::{sigprocmask, SigmaskHow, Signal, SigSet};
+use nix::sys::signalfd::SignalFd;
+use tracing_subscriber::prelude::__tracing_subscriber_SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
 
-    println!("The lowest carbon intensity period is: {:#?}", forecast.lowest());
+/// The token associated with either SIGHUP (for reloading cfg), or a terminating signal (SIGTERM, SIGINT).
+const SIGNAL_TOKEN: Token = Token(1);
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let registry = tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer().with_target(false));
+    
+    match tracing_journald::layer() {
+        Ok(layer) => {
+            registry.with(layer).init();
+        }
+        // journald is typically available on Linux systems, but nowhere else. Portable software
+        // should handle its absence gracefully.
+        Err(e) => {
+            registry.init();
+            tracing::error!("couldn't connect to journald: {}", e);
+        }
+    }
+
+    let mut poll = Poll::new()?;
+    let mut events = Events::with_capacity(10); // 10 simultaneous, not 10 total registered
+    
+    let mut mask = SigSet::empty();
+    mask.add(Signal::SIGHUP);
+    mask.add(Signal::SIGTERM);
+    mask.add(Signal::SIGINT);
+    sigprocmask(SigmaskHow::SIG_BLOCK, Some(&mask), None)?;
+    let sfd = SignalFd::new(&mask)?;
+
+    poll.registry().register(
+        &mut SourceFd(&sfd.as_raw_fd()),
+        SIGNAL_TOKEN,
+        Interest::READABLE
+    )?;
+
+    // Leave this label for easy debugging with rust-analyzer.
+    '_poll_events: loop {
+        poll.poll(&mut events, None)?;
+
+        for event in events.iter() {
+            match event.token() {
+                SIGNAL_TOKEN => {
+                    // Internal loop in case of queued signals (someone spamming SIGHUP?).
+                    'signal: loop {
+                        match sfd.read_signal() {
+                            Ok(Some(sig)) => {
+                                // It not valid signal, do nothing.
+                                if let Ok(signal) = Signal::try_from(sig.ssi_signo as i32) {
+                                    match signal {
+                                        Signal::SIGHUP => tracing::info!("SIGHUP Received, rebooting cfg..."),
+                                        Signal::SIGINT | Signal::SIGTERM => {
+                                            tracing::warn!("Terminating signal received. Shutting down cleanly...");
+                                            return Ok(())
+                                        },
+                                        // Shouldn't really be receiving other signals.
+                                        _ => {}
+                                    }
+                                }
+                            },
+                            Ok(None) => {
+                                // No more signals waiting/partial read, return to main loop.
+                                break 'signal;
+                            },
+                            Err(e) => {
+                                /* Let systemd handle restart: error beyond control of this process, 
+                                so might as well just let systemd restart us to re-register the 
+                                signal events. */
+                                eprintln!("{e}");
+                                std::process::exit(1);
+                            }
+                        }
+                    }
+                },
+                // Put schedule refresh && process spawning (D-Bus/varlink) logic here.
+                _ => unreachable!()
+            }
+        }
+    }
+
+    // let forecast = forecast::Forecast::fetch_fw_24h_postcode(
+    //     "YO1", time::UtcDateTime::now()
+    // ).unwrap();
+
+    // println!("{:#?}", forecast);
 }
